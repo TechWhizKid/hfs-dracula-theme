1cfdfb496e2a68189f933567a0faa0d42183c8ae
TechWhizKid <nomans.protonmail@proton.me>
2024-07-02 02:36:11 +0600

    v2.0

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,5 +1,7 @@
-exports.version = 1.0
+exports.version = 2.0
 exports.description = "Dracula theme by TechWhizKid"
+exports.isTheme = "dark"
+exports.preview = "https://github.com/TechWhizKid/hfs-dracula-theme/raw/main/preview.png"
 exports.apiRequired = 1
 exports.repo = "TechWhizKid/hfs-dracula-theme"
-exports.frontend_css = 'style.css'
+exports.frontend_css = "style.css"
